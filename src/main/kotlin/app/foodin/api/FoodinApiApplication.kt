deb68d2b4f5adcbd4e7b3e59fe1f55a98a7c79b3
foodin <foodin@foodin.app>
2018-12-10 00:44:25 +0900

    project start

---
+++ src/main/kotlin/app/foodin/api/FoodinApiApplication.kt
@@ -0,0 +1,13 @@
+package app.foodin.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FoodinApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<FoodinApiApplication>(*args)
+
+    //start
+}
