f1f92247c6c04078efdb11a41af2de1c4f1f602a
kimyoon21 <kimyoon21@gmail.com>
2019-02-06 17:27:59 +0900

    BaseGateway, BaseService 추가해서 적용

--- core/src/main/kotlin/app/foodin/core/service/FoodService.kt
+++ core/src/main/kotlin/app/foodin/core/service/FoodService.kt
@@ -1,29 +1,54 @@
 package app.foodin.domain.user
 
 import app.foodin.domain.food.Food
+import app.foodin.domain.food.FoodRegRequest
+import app.foodin.domain.writable.UserWritableInterface
 import org.slf4j.LoggerFactory
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
-import org.springframework.data.jpa.domain.Specification
 import org.springframework.stereotype.Service
-
-interface UserWritableService{
-    fun findAll(spec:Specification<*>?,pageable: Pageable): Page<Food>
-    fun saveFrom(food: Food) : Food
-}
 
 @Service
 class FoodService(
         private val foodGateway: FoodGateway
-) : UserWritableService{
-    override fun saveFrom(food: Food): Food {
-        return foodGateway.saveFrom(food)
-    }
+) : BaseService<Food>(foodGateway), UserWritableInterface {
 
     private val logger = LoggerFactory.getLogger(FoodService::class.java)
 
-    override fun findAll(spec: Specification<*>?, pageable: Pageable): Page<Food> {
-        return foodGateway.findAll(spec,pageable)
+    fun findByName(name: String) : Food? {
+        return foodGateway.findByName(name)
+    }
+
+    fun makeNewFoodOrMergeByFoodRegRequest(foodRegRequest: FoodRegRequest) {
+        var food = foodGateway.findByName(foodRegRequest.name)
+        if (food == null) {
+            food = Food(name=foodRegRequest.name, categoryId = foodRegRequest.categoryId)
+        }
+
+        applyFoodRegRequestToFood(foodRegRequest,food)
+        // TODO 생성은 ok 기존꺼 수정은 어떻게?
+        foodGateway.saveFrom(food)
+    }
+
+    fun applyFoodRegRequestToFood(foodRegRequest: FoodRegRequest, food: Food): Food {
+        food.let {
+
+            it.companyId = foodRegRequest.companyId
+            if (it.minPrice > foodRegRequest.price) {
+                it.minPrice = foodRegRequest.price
+            }
+            if (it.maxPrice < foodRegRequest.price) {
+                it.maxPrice = foodRegRequest.price
+            }
+            if(foodRegRequest.summary?.length ?: 0 > it.summary?.length ?: 0){
+                it.summary = foodRegRequest.summary
+            }
+            if (it.mainPhotoUri == null) {
+                it.mainPhotoUri = foodRegRequest.mainPhotoUri
+            }
+            it.tagList.addAll(foodRegRequest.tagList)
+            it.photoList.addAll(foodRegRequest.photoList)
+
+            return it
+        }
     }
 
 }
