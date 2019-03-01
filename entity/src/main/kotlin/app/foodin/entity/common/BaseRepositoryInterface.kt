9c7ea1035c919f02c7c076dd6df116b82dec1782
Kim yoon <kimyoon21@gmail.com>
2019-03-01 20:44:07 +0900

    노레파지토리빈 어노테이션 추가

--- entity/src/main/kotlin/app/foodin/entity/common/BaseRepositoryInterface.kt
+++ entity/src/main/kotlin/app/foodin/entity/common/BaseRepositoryInterface.kt
@@ -2,5 +2,7 @@ package app.foodin.entity.common
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import org.springframework.data.repository.NoRepositoryBean
 
+@NoRepositoryBean
 interface BaseRepositoryInterface<E> : JpaRepository<E,Long>, JpaSpecificationExecutor<E>
