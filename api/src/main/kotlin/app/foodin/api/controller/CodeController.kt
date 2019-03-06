c999401d061bbb166a67b24855278961ded6906e
Kim yoon <kimyoon21@gmail.com>
2019-03-06 23:34:08 +0900

    코드 조회 기본 페이지값 100

--- api/src/main/kotlin/app/foodin/api/controller/CodeController.kt
+++ api/src/main/kotlin/app/foodin/api/controller/CodeController.kt
@@ -4,6 +4,7 @@ import app.foodin.common.result.ResponseResult
 import app.foodin.core.service.CodeService
 import app.foodin.domain.code.CodeFilter
 import org.springframework.data.domain.Pageable
+import org.springframework.data.web.PageableDefault
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -16,7 +17,7 @@ class CodeController(
 
     @GetMapping
     fun getAll(
-        pageable: Pageable,
+        @PageableDefault(page = 0,size = 100) pageable: Pageable,
         codeFilter: CodeFilter
     ): ResponseResult {
 
