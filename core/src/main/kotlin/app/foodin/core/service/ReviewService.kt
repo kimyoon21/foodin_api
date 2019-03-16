83716e281c9e9cd33c34c3f0816e695da072e275
Kim yoon <kimyoon21@gmail.com>
2019-03-16 13:52:40 +0900

    리뷰 한사람이 한 음식에 한번만 가능하게

--- core/src/main/kotlin/app/foodin/core/service/ReviewService.kt
+++ core/src/main/kotlin/app/foodin/core/service/ReviewService.kt
@@ -1,5 +1,7 @@
 package app.foodin.domain.user
 
+import app.foodin.common.exception.CommonException
+import app.foodin.common.exception.EX_ALREADY_EXISTS_WHAT
 import app.foodin.core.gateway.ReviewGateway
 import app.foodin.core.service.UserService
 import app.foodin.domain.review.Review
@@ -21,6 +23,11 @@ class ReviewService(
     private val logger = LoggerFactory.getLogger(ReviewService::class.java)
 
     fun saveFrom(createReq: ReviewCreateReq): Review {
+        // 기 데이터 확인
+        // 이미 작성한 리뷰(평점)이 있으면 무조건 update
+        gateway.findByWriteUserIdAndFoodId(createReq.writeUserId,createReq.foodId)?.let {
+            throw CommonException(EX_ALREADY_EXISTS_WHAT,"word.review")
+        }
 
         // 푸드 주입
         val food = foodService.findById(createReq.foodId)
