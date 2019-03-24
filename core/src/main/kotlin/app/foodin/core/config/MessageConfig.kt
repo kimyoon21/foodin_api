cdaa5283d20e4b3184d7209ca529530bbe2badc1
Kim yoon <kimyoon21@gmail.com>
2019-03-25 02:58:41 +0900

    메시지 설정 추가

---
+++ core/src/main/kotlin/app/foodin/core/config/MessageConfig.kt
@@ -0,0 +1,27 @@
+package app.foodin.core.config
+
+import org.springframework.context.MessageSource
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.support.ReloadableResourceBundleMessageSource
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean
+
+
+@Configuration
+class MessageConfig{
+
+    @Bean
+    fun messageSource(): MessageSource {
+        val messageSource = ReloadableResourceBundleMessageSource()
+
+        messageSource.setBasename("classpath:messages")
+        messageSource.setDefaultEncoding("UTF-8")
+        return messageSource
+    }
+    @Bean
+    fun getValidator(): LocalValidatorFactoryBean {
+        val bean = LocalValidatorFactoryBean()
+        bean.setValidationMessageSource(messageSource())
+        return bean
+    }
+}
