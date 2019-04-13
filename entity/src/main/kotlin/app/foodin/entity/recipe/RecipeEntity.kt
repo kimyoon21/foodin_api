5aaa795958428e5aad3c2855f2e2252e51b45c56
Kim yoon <kimyoon21@gmail.com>
2019-04-13 20:10:41 +0900

    writerId 로 레시피 기본 형태 세팅

--- entity/src/main/kotlin/app/foodin/entity/recipe/RecipeEntity.kt
+++ entity/src/main/kotlin/app/foodin/entity/recipe/RecipeEntity.kt
@@ -4,11 +4,10 @@ import app.foodin.domain.recipe.Recipe
 import app.foodin.entity.common.BaseEntity
 import javax.persistence.Entity
 import javax.persistence.Table
-import kotlin.String
 
 @Entity
 @Table(name = "recipe")
-data class RecipeEntity(val field1: String) : BaseEntity<Recipe>() {
+data class RecipeEntity(val writerId: Long) : BaseEntity<Recipe>() {
     override fun toDomain(): Recipe {
                 TODO("not implemented")
     }
