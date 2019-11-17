28096f6ca04075ace8470bbdbe59280a46c8c128
Kim yoon <kimyoon21@gmail.com>
2019-11-17 17:35:24 +0900

    apple login 관련 수정 (하지만 실패... 그냥 id_token 을 accessToken 으로 던져서 처리하기로)

--- api/src/main/kotlin/app/foodin/api/controller/AppleAuthController.kt
+++ api/src/main/kotlin/app/foodin/api/controller/AppleAuthController.kt
@@ -8,10 +8,7 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.http.MediaType
 import org.springframework.util.LinkedMultiValueMap
-import org.springframework.web.bind.annotation.ModelAttribute
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.client.RestTemplate
 
@@ -22,11 +19,20 @@ class AppleAuthController() {
     private val logger = LoggerFactory.getLogger(AppleAuthController::class.java)
 
     @PostMapping
-    fun callback(@ModelAttribute appleResult: AppleAuthResult): ResponseResult {
+    fun callbackFromWeb(@ModelAttribute appleResult: AppleAuthResult): ResponseResult {
+        return handling(appleResult,"")
+    }
 
+    @PostMapping("/app")
+    fun callbackFromApp(@RequestBody appleResult: AppleAuthResult): ResponseResult {
+        return handling(appleResult, "/app")
+    }
+
+
+    fun handling(appleResult: AppleAuthResult, path: String): ResponseResult {
         val clientId = "app.foodin.client"
         val clientSecret = "eyJhbGciOiJFUzI1NiIsImtpZCI6Iko3NVU2MlkyWjcifQ.eyJpc3MiOiJHNDdWRDRSNzc3IiwiYXVkIjoiaHR0cHM6Ly9hcHBsZWlkLmFwcGxlLmNvbSIsInN1YiI6ImFwcC5mb29kaW4uY2xpZW50IiwiaWF0IjoxNTcyMTE3MTY4LCJleHAiOjE1ODA3NTcxNjh9.h-cZvEpYU3MZ0uF7IHKzOwKuxZ0h271OiQIlXfBWHNCqWdL-gMp3pj1L0l-mwjGjTCU1SGKwqlqGL3KesgHKTQ"
-        val redirectUri = "https://foodin.app/apple-auth"
+        val redirectUri = "https://foodin.app/apple-auth$path"
         val restTemplate = RestTemplate()
 
         val endpoint = "https://appleid.apple.com/auth/token"
@@ -59,6 +65,6 @@ class AppleAuthController() {
 }
 
 data class AppleAuthResult(
-    val code: String,
-    val id_token: String
+        val code: String,
+        val id_token: String
 )
