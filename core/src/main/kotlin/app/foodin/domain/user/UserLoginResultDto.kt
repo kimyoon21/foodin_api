14c981678b10de2631154e38085b33c167366313
Kim yoon <kimyoon21@gmail.com>
2020-01-13 01:32:55 +0900

    refesh 토큰 오타 수정

--- core/src/main/kotlin/app/foodin/domain/user/UserLoginResultDto.kt
+++ core/src/main/kotlin/app/foodin/domain/user/UserLoginResultDto.kt
@@ -4,24 +4,24 @@ import app.foodin.common.enums.SnsType
 import java.sql.Timestamp
 
 class UserLoginResultDto(
-    val id: Long,
-    val email: String?,
-    val realName: String,
-    val nickName: String,
-    val snsType: SnsType,
-    var accessToken: String,
-    var refeshToken: String,
-    var expireTime: Timestamp
+        val id: Long,
+        val email: String?,
+        val realName: String,
+        val nickName: String,
+        val snsType: SnsType,
+        var accessToken: String,
+        var refreshToken: String,
+        var expireTime: Timestamp
 
 ) {
 
-    constructor(user: User, accessToken: String, refeshToken: String, expireTime: Timestamp) : this(
+    constructor(user: User, accessToken: String, refreshToken: String, expireTime: Timestamp) : this(
             id = user.id,
             email = user.email,
             realName = user.realName,
             nickName = user.nickName ?: user.realName,
             snsType = user.snsType,
             accessToken = accessToken,
-            refeshToken = refeshToken,
+            refreshToken = refreshToken,
             expireTime = expireTime)
 }
